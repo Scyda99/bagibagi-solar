243113100caaabc0672244e332568e5dd956b632
Scyda99 <putranst06@gmail.com>
2026-06-10 14:27:32 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+let latestDonation = null;
+
+app.post("/webhook", (req, res) => {
+
+    latestDonation = {
+        id: Date.now(),
+        name: req.body.name || "Anonim",
+        amount: req.body.amount || 0,
+        message: req.body.message || ""
+    };
+
+    console.log(latestDonation);
+
+    res.status(200).json({
+        success: true
+    });
+});
+
+app.get("/latest-donation", (req, res) => {
+    res.json(latestDonation || {});
+});
+
+app.get("/", (req, res) => {
+    res.send("Server Bagibagi Roblox Aktif");
+});
+
+app.listen(process.env.PORT || 3000);
